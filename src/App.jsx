3a536a6635ccd30d4629210290f8e558aadb2c0c
jamesarrojo.01 <jamesarrojo.01@gmail.com>
2022-09-28 17:28:11 +0800

    polished UI

--- src/App.jsx
+++ src/App.jsx
@@ -208,7 +208,7 @@ function App() {
   if (hasClaimedNFT) {
     return (
       <div className='member-page'>
-        <h1>🍪DAO Member Page</h1>
+        <h1>💻DAO Member Page</h1>
         <p>Congratulations on being a member</p>
         <div>
           <div>
